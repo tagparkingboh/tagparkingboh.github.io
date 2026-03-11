eb562dc16408d1e79e2900f6126f2ea6f7e55715
QA Orca <qaorca@QAs-MacBook-Pro.local>
2026-03-10 21:02:47 -0700

    Set offset to -250px

--- tag-website/src/HomePage.jsx
+++ tag-website/src/HomePage.jsx
@@ -53,7 +53,7 @@ function HomePage() {
         const element = document.getElementById(id)
         if (element) {
           window.scrollTo({
-            top: element.offsetTop - 300,
+            top: element.offsetTop - 250,
             behavior: 'smooth'
           })
         }
